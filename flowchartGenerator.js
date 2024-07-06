b25ba1cf8e5af259ffa464edb2a52183bf4dcf81
Easy Learn Java <easylearnjava4u@gmail.com>
2024-07-06 08:57:30 -0500

    Update flowchartGenerator.js

--- flowchartGenerator.js
+++ flowchartGenerator.js
@@ -1,47 +1,41 @@
-const flowchartGenerator = (function() {
-    function generateFlowchart(parsedLog) {
-        let flowchartText = '';
+export function generateFlowchart(parsedLog) {
+    let flowchartText = '';
 
-        if (parsedLog.validations.length > 0) {
-            flowchartText += `
+    if (parsedLog.validations.length > 0) {
+        flowchartText += `
 +-----------------------------+
 |      Validation Rules       |
 +-----------------------------+
 ${parsedLog.validations.map(validation => `| ${validation.ruleName}         `).join('\n')}
 +-----------------------------+
 `;
-        }
+    }
 
-        if (parsedLog.duplicateRules.length > 0) {
-            if (flowchartText) {
-                flowchartText += '              |\n              v\n';
-            }
-            flowchartText += `
+    if (parsedLog.duplicateRules.length > 0) {
+        if (flowchartText) {
+            flowchartText += '              |\n              v\n';
+        }
+        flowchartText += `
 +-----------------------------+
 |    Duplicate Detection      |
 +-----------------------------+
 ${parsedLog.duplicateRules.map(rule => `| ${rule.ruleName}               `).join('\n')}
 +-----------------------------+
 `;
-        }
-
-        if (parsedLog.triggers.length > 0) {
-            if (flowchartText) {
-                flowchartText += '              |\n              v\n';
-            }
-            flowchartText += `
-+-----------------------------+
-|         Triggers            |
-+-----------------------------+
-${parsedLog.triggers.map(trigger => `| ${trigger.triggerName} (${trigger.triggerEvent})         `).join('\n')}
-+-----------------------------+
-`;
-        }
-
-        return flowchartText;
     }
 
-    return {
-        generateFlowchart: generateFlowchart
-    };
-})();
+    if (parsedLog.triggerDetails) {
+        if (flowchartText) {
+            flowchartText += '              |\n              v\n';
+        }
+        flowchartText += `
++-----------------------------+
+|        Trigger Event        |
++-----------------------------+
+| ${parsedLog.triggerDetails.triggerName} (${parsedLog.triggerDetails.triggerEvent}) |
++-----------------------------+
+`;
+    }
+
+    return flowchartText.trim();
+}
