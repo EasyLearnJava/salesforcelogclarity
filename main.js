10f8210c902c9b8b6cc3fbf1f87f96167477312a
Easy Learn Java <easylearnjava4u@gmail.com>
2024-07-06 08:56:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// Import the necessary functions from the other files
 import { parseLog } from './parser.js';
 import { generateSummary } from './summaryGenerator.js';
 import { generateFlowchart } from './flowchartGenerator.js';
 
-// Event listener for the Analyze Log button
 document.getElementById('analyzeLogButton').addEventListener('click', analyzeLog);
 
 function analyzeLog() {
